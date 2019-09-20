9051f222384e51d2563e7d0bb191d0c1a7b0aa89
deema1r <deemaalrsheed@gmail.com>
2019-09-21 00:21:28 +0300

    main commet

--- apps/app1/views.py
+++ apps/app1/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 #test
+
+def index(request):
+    
+    url = 'https://www.virustotal.com/vtapi/v2/url/scan'
+
+    params = {'apikey': '6c72dc928ae0eb65656cbdc4a9898ba4b8ba8073acdcb45659911648640b5fd4', 'url':'www.google.com'}
+
+    response = requests.post(url, data=params)
+
+    print(response.json())
